9073a89a1d466b67bdf1d584410925c9107254a4
Koray Ozkal <koryozkal@gmail.com>
2020-10-18 22:03:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+
+class Cat {
+  constructor(name, sex) {
+    
+    
+    
+  }
+}
+ 
+class Dog {
+   constructor(name, sex) {
+    
+    
+    
+  }
+}
+ 
+ 
+class Bird {
+  constructor(name, sex) {
+    
+    
+  }
+}
+ 
